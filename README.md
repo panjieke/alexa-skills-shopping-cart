faa2ba2a6537e39b33f74eedbef9315955776c8c
Jieke Pan <jpan@mobiquityinc.com>
2017-04-02 09:21:29 +0200

    Edited README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # alexa-skills-shopping-cart
+It is a simple alexa skill spike project in Java, which I have been working on.
+The main goal is decoupling it into a easily configurable project, that allows you to build quickly alexa skill in Java.
+
+**Note:**
+- The current version is a spike, so it is**not done yet in TDD**
+- Shopping car is just an example
--- src/main/java/panjieke/sample/alexa/shopping/ShoppingSpeechlet.java
+++ src/main/java/panjieke/sample/alexa/shopping/ShoppingSpeechlet.java
@@ -1,5 +1,6 @@
 package panjieke.sample.alexa.shopping;
 
+import com.amazon.speech.ui.Reprompt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,11 +10,16 @@ import com.amazon.speech.speechlet.*;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.SimpleCard;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShoppingSpeechlet implements SpeechletV2 {
 
     private static final Logger log = LoggerFactory.getLogger(ShoppingSpeechlet.class);
     private static final String SHOPPING_CART = "Shopping Cart";
     private static final String SHOPPING_CART_INTENT = "ShoppingCartIntent";
+    private static final String STOP_INTENT = "AMAZON.StopIntent";
+    private Map<String, Integer> shoppingCart = new HashMap<String, Integer>(){{ put("banana", 3); put("apple", 5); }};
 
     @Override
     public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> speechletRequestEnvelope) {
@@ -42,8 +48,13 @@ public class ShoppingSpeechlet implements SpeechletV2 {
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : null;
 
-        return getSimpleSpeechletResponse(intentName);
-
+        if (SHOPPING_CART_INTENT.equals(intentName)) {
+            return getRepromptSpeechletResponse(getMyShoppingList(), "Do you want to add something else?");
+        } else if (STOP_INTENT.equals(intentName)) {
+            return getGoodByeResponse();
+        } else {
+            return getUnknownCommandResponse();
+        }
     }
 
     @Override
@@ -53,7 +64,49 @@ public class ShoppingSpeechlet implements SpeechletV2 {
                 speechletRequestEnvelope.getSession().getSessionId());
     }
 
-    private SpeechletResponse createSimpleSpeechletResponse(String speechText) {
+    private SimpleCard createCard(String title, String speechText) {
+        SimpleCard card = new SimpleCard();
+        card.setTitle(title);
+        card.setContent(speechText);
+
+        return card;
+    }
+
+    private PlainTextOutputSpeech createSpeech(String speechText) {
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return speech;
+    }
+
+    private SpeechletResponse getRepromptSpeechletResponse(String speechText, String repromptText) {
+        SimpleCard card = createCard(SHOPPING_CART, speechText);
+        PlainTextOutputSpeech speech = createSpeech(speechText);
+
+        PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
+        repromptSpeech.setText(repromptText);
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptSpeech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+    private SpeechletResponse getWelcomeResponse() {
+        return getRepromptSpeechletResponse("Welcome to Shopping Cart", "You can ask for your shopping list or add items");
+    }
+
+    private String getMyShoppingList() {
+        StringBuilder list = new StringBuilder().append("This is your usual shopping list:");
+        for (String article : shoppingCart.keySet()) {
+            list.append(shoppingCart.get(article) + " " + article + " ");
+        }
+
+        list.append("Do you want to add something else?");
+
+        return list.toString();
+    }
+
+    private SpeechletResponse getSimpleSpeechletResponse(String speechText) {
         SimpleCard card = new SimpleCard();
         card.setTitle(SHOPPING_CART);
         card.setContent(speechText);
@@ -64,31 +117,11 @@ public class ShoppingSpeechlet implements SpeechletV2 {
         return SpeechletResponse.newTellResponse(speech, card);
     }
 
-    private SpeechletResponse getSimpleSpeechletResponse(String intentName) {
-        if (SHOPPING_CART_INTENT.equals(intentName)) {
-            return getOpenShoppingCartResponse();
-        } else {
-            return getUnknownCommandResponse();
-        }
-    }
-
-    private SpeechletResponse getRepromptSpeechletResponse(String intentName) {
-        if (SHOPPING_CART_INTENT.equals(intentName)) {
-            return getOpenShoppingCartResponse();
-        } else {
-            return getUnknownCommandResponse();
-        }
-    }
-
-    private SpeechletResponse getWelcomeResponse() {
-        return createSimpleSpeechletResponse("Welcome to Shopping Cart");
-    }
-
-    private SpeechletResponse getOpenShoppingCartResponse() {
-        return createSimpleSpeechletResponse("This is your shopping cart");
+    private SpeechletResponse getGoodByeResponse() {
+        return getSimpleSpeechletResponse("ByeBye");
     }
 
     private SpeechletResponse getUnknownCommandResponse() {
-        return createSimpleSpeechletResponse("Sorry, I did not understand your command");
+        return getSimpleSpeechletResponse("I did not understand your command");
     }
 }
